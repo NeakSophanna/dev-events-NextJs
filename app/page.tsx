a0f2e3cb3075b59e8afd6ebdc949710e787d5572
NeakSophanna <neak.sophanna011@gmail.com>
2026-03-03 22:07:21 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+import ExploreBtn from "@/components/ExploreBtn";
+import EventCard from "@/components/EventCard";
+import {events,EventItem} from "@/lib/constants"
+const Page = () => {
+    return (
+        <section>
+            <h1 className="text-center">
+                The Hub For Every Dev <br />
+                Event You Can&#39;t Miss
+            </h1>
+            <p className="text-center mt-5">Hackathons, Meetups,and Conferences, All in One Place</p>
+            <ExploreBtn />
+            <div className="mt-20 space-y-7">
+                <h3>Features</h3>
+
+                <ul className="events">
+                    {
+                        events.map((event:EventItem) => (
+                            <li key={event.title}>
+                                <EventCard {...event}></EventCard>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </section>
+    )
+}
+export default Page
---
+++ lib/constants.ts
@@ -0,0 +1,59 @@
+export type EventItem={
+    image:string,
+    title:string,
+    slug:string,
+    location:string,
+    date:string,
+    time:string,
+}
+
+export const events: EventItem[] = [
+    {
+        image: "/images/event1.png",
+        title: "React Summit US 2025",
+        slug: "react-summit-us-2025",
+        location: "San Francisco, CA, USA",
+        date: "2025-11-07",
+        time: "9:00 AM",
+    },
+    {
+        image: "/images/event2.png",
+        title: "React Summit US 2025",
+        slug: "react-summit-us-2025",
+        location: "San Francisco, CA, USA",
+        date: "2025-11-07",
+        time: "9:00 AM",
+    },
+    {
+        image: "/images/event3.png",
+        title: "React Summit US 2025",
+        slug: "react-summit-us-2025",
+        location: "San Francisco, CA, USA",
+        date: "2025-11-07",
+        time: "9:00 AM",
+    },
+    {
+        image: "/images/event4.png",
+        title: "React Summit US 2025",
+        slug: "react-summit-us-2025",
+        location: "San Francisco, CA, USA",
+        date: "2025-11-07",
+        time: "9:00 AM",
+    },
+    {
+        image: "/images/event5.png",
+        title: "React Summit US 2025",
+        slug: "react-summit-us-2025",
+        location: "San Francisco, CA, USA",
+        date: "2025-11-07",
+        time: "9:00 AM",
+    },
+    {
+        image: "/images/event6.png",
+        title: "React Summit US 2025",
+        slug: "react-summit-us-2025",
+        location: "San Francisco, CA, USA",
+        date: "2025-11-07",
+        time: "9:00 AM",
+    },
+]
